be9f61fa3100a15a95fe6f98bac9d08df1111e06
Sofia Carballo <sofia.carballo@live.com>
2021-01-13 15:54:32 +0100

    Adds test to check number of testcases input

--- hackerrank/Day-6-lets-review/test_characterSeparator.py
+++ hackerrank/Day-6-lets-review/test_characterSeparator.py
@@ -1,10 +1,13 @@
 from characterSeparator import CharacterSeparator
 
-# def test_whatever():
-    # f = classname()
-    # assert f.say(result) == 'expected value'
-
 def test_input_string_is_in_valid_length_range():
     string = "Hello"
     characterSeparator = CharacterSeparator()
     assert characterSeparator.check_string_length(string) == True
+
+def test_number_of_testcases():
+    characterSeparator = CharacterSeparator()
+    assert characterSeparator.check_number_of_testcases(2) == True
+    assert characterSeparator.check_number_of_testcases(11) == False
+    assert characterSeparator.check_number_of_testcases(0) == False
+    assert characterSeparator.check_number_of_testcases("a") == False
