190b30608e27b0268f8dea234fab378100ea66de
Elena-GHub <cabjelen@yahoo.es>
2021-01-13 11:16:58 +0100

    Moved Fizzbuzz folder to hackerrank folderfor allow tests to run

---
+++ hackerrank/FizzBuzz/FizzBuzz.py
@@ -0,0 +1,9 @@
+class FizzBuzz:
+    def say(self, number):
+        if number % 3 == 0 and number % 5 == 0:
+            return 'FizzBuzz'
+        if number % 3 == 0:
+            return 'Fizz'
+        if number % 5 == 0:
+            return 'Buzz'
+        return number
