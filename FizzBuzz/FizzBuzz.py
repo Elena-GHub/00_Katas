2bcc12213471fd001507f8d8e3bbff81b7cad89a
Elena-GHub <cabjelen@yahoo.es>
2021-01-13 11:20:01 +0100

    recommitting as for some reason file deletion were ignored in previous commit

--- FizzBuzz/FizzBuzz.py
+++
@@ -1,9 +0,0 @@
-class FizzBuzz:
-    def say(self, number):
-        if number % 3 == 0 and number % 5 == 0:
-            return 'FizzBuzz'
-        if number % 3 == 0:
-            return 'Fizz'
-        if number % 5 == 0:
-            return 'Buzz'
-        return number
